7227cd789a25e3b18d1a5482cdf0c22207470e3f
Ramesh Gehlot <rameshgehlot2210@gmail.com>
2026-06-02 23:05:54 +0530

    Refactor FastAPI example and remove comments
    Comment out installation instructions and clean up code.

--- fastapi.py
+++ fastapi.py
@@ -1,16 +1,13 @@
 # FastAPI Example 
-'''
-python 3.6+
-pip install fastapi uvicorn
-''' 
+# python 3.6+
+# pip install fastapi uvicorn
+
 from fastapi import FastAPI
 
 app = FastAPI() 
 @app.get('/')
 def hello_world():
     return {'Hello':'World'} 
-# To run the server: uvicorn filename:app --reload 
-
 
 # 2nd
 from fastapi import FastAPI 
@@ -20,5 +17,3 @@ def read_word():
     return [{"name": "Ramesh"}, {"name": "Suresh"}]
 
 # To run the server: uvicorn filename:app  --reload 
-
-
